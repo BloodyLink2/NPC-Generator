e638005d2ccfedcd6e8891e4aafb0dd393680d16
Angelo <bloodygaia@outlook.de>
2019-06-13 00:32:03 +0200

    Update global.ts

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,5 @@
 export class Global {
-    public isLocal:boolean = true;
+    public isLocal:boolean = false;
     public localUrl:string = "http://localhost:81/NPC-Api/api/crud/"
     public hostUrl:string = "api/crud/"
 }
